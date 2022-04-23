819818e07058601bd8904e0a66e898494530cdf1
bigbook <bigbook.pl@gmail.com>
2022-04-23 11:50:02 +0200

    Zadanie 2 / dzien 3 czesc 2

--- src/main/java/pl/coderslab/controller/BookFormController.java
+++ src/main/java/pl/coderslab/controller/BookFormController.java
@@ -3,6 +3,7 @@ package pl.coderslab.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import pl.coderslab.dao.PublisherDao;
 import pl.coderslab.entity.Book;
 import pl.coderslab.entity.Publisher;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -24,24 +26,25 @@ public class BookFormController {
     PublisherDao publisherDao;
 
     @RequestMapping("/add-book")
-    public String showBookForm(Model model){
-
+    public String showBookForm(Model model) {
         model.addAttribute("book", new Book());
 
         return "add-book-form";
     }
 
     @RequestMapping(value = "/add-book", method = RequestMethod.POST)
-    @ResponseBody
-    public String saveBookForm(Book book){
+    public String saveBookForm(@Valid Book book, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "add-book-form";
+        }
 
         bookDao.add(book);
-
-        return "Book was added "+book.getId();
+        return "redirect:/book/list";
     }
 
     @ModelAttribute("publishers")
-    public List<Publisher> getPublishers(){
+    public List<Publisher> getPublishers() {
         return publisherDao.findAll();
     }
 
--- src/main/webapp/WEB-INF/views/add-book-form.jsp
+++ src/main/webapp/WEB-INF/views/add-book-form.jsp
@@ -11,14 +11,39 @@
 <html>
 <head>
     <title>Add Book Form</title>
+    <style>
+        .error {
+            color: red;
+        }
+    </style>
 </head>
 <body>
     <%--@elvariable id="book" type="pl.coderslab.entity.Book"--%>
     <form:form modelAttribute="book" method="post">
-        Title: <form:input path="title"/>
-        Rating: <form:input path="rating" />
-        Description <form:textarea path="description"/>
+        <label for="title">Title</label>
+        <form:input path="title"/>
+        <form:errors path="title" cssClass="error" />
+        <br/>
+
+        <label for="rating">Rating:</label>
+        <form:input path="rating" />
+        <form:errors path="rating" cssClass="error" />
+        <br/>
+
+        <label for="pages">Pages:</label>
+        <form:input path="pages" />
+        <form:errors path="pages" cssClass="error"/>
+        <br/>
+
+        <label for="description">Description</label>
+        <form:textarea path="description"/>
+        <form:errors path="description" cssClass="error"/>
+        <br/>
+
+        <label for="publisher">Publisher:</label>
         <form:select path="publisher" items="${publishers}" itemLabel="name" itemValue="id"/>
+        <form:errors path="publisher" cssClass="error"/>
+
         <input type="submit">
     </form:form>
 </body>
