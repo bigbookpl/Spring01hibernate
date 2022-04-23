fc39e6b740cf7a76f9ed5aeb1141ef9cb74c2992
bigbook <bigbook.pl@gmail.com>
2022-04-23 09:54:53 +0200

    Zadanie 1 / dzien 3

--- src/main/java/pl/coderslab/app/AppConfig.java
+++ src/main/java/pl/coderslab/app/AppConfig.java
@@ -9,13 +9,16 @@ import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import pl.coderslab.converter.PublisherConverter;
 
 import javax.persistence.EntityManagerFactory;
 import java.nio.charset.Charset;
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
@@ -34,6 +37,14 @@ public class AppConfig implements WebMvcConfigurer {
         JpaTransactionManager jpaTransactionManager = new JpaTransactionManager(entityManagerFactory);
         return jpaTransactionManager;
     }
+
+    @Bean(name="localeResolver")
+    public LocaleContextResolver getLocaleContextResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pl","PL"));
+        return localeResolver;
+    }
+
     //to są poniżej beany potrzebne do WEB MVC, nie wymaga ich Hibernate
     @Bean
     public StringHttpMessageConverter stringHttpMessageConverter() {
